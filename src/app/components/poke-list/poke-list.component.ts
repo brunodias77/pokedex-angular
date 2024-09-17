2d9a5c64d7d97d332d59193cb48b6bce110a0100
brunodias77 <brunohdias95@gmail.com>
2024-09-17 10:31:40 -0300

    feat: Atualizar estilos e layout da aplicação
    Atualiza o arquivo poke-list.component.html para adicionar botões de navegação para paginação e comentar código desnecessário.
    Atualiza o arquivo poke-list.component.ts para adicionar funcionalidade de paginação e filtragem de pokémons.
    Atualiza o arquivo poke-api.service.ts para adicionar método de listagem de pokémons com paginação.

--- src/app/components/poke-list/poke-list.component.ts
+++ src/app/components/poke-list/poke-list.component.ts
@@ -16,37 +16,103 @@ export class PokeListComponent implements OnInit {
   public filteredPokemons: any[] = [];
   public isLoading: boolean = true;
   public searchTerm: string = '';
+  public offset: number = 0; // Controle da paginação
+  public limit: number = 21; // Número de pokémons por página
 
   constructor(
     private pokeApiService: PokeApiService,
-    private searchService: SearchService // Injetar o SearchService
+    private searchService: SearchService
   ) {}
 
   ngOnInit(): void {
-    // Subscrição para obter todos os Pokémons
-    this.pokeApiService.apiListAllPokemons.subscribe((data) => {
-      this.pokemons = data.results;
-      this.filteredPokemons = this.pokemons; // Inicialmente, todos os Pokémons são exibidos
-      this.isLoading = false;
-    });
+    this.getPokemons();
 
-    // Subscrição para receber atualizações do termo de busca
     this.searchService.currentSearchTerm.subscribe((term) => {
       this.searchTerm = term;
-      console.log('Estou na  PokeList:', this.searchTerm); // Log para verificar
-      this.filterPokemons(); // Filtrar a lista de Pokémons com base no termo de busca
+      this.filterPokemons();
     });
   }
 
-  // Método para filtrar os Pokémons com base no searchTerm
+  // Função para obter os Pokémons da página atual
+  getPokemons(): void {
+    this.isLoading = true;
+    this.pokeApiService
+      .getPokemonList(this.offset, this.limit)
+      .subscribe((data) => {
+        this.pokemons = data.results;
+        this.filteredPokemons = this.pokemons;
+        this.isLoading = false;
+      });
+  }
+
+  // Método para ir para a próxima página
+  nextPage(): void {
+    this.offset += this.limit;
+    this.getPokemons();
+  }
+
+  // Método para voltar à página anterior
+  prevPage(): void {
+    if (this.offset > 0) {
+      this.offset -= this.limit;
+      this.getPokemons();
+    }
+  }
+
   filterPokemons(): void {
     if (this.searchTerm) {
       this.filteredPokemons = this.pokemons.filter((pokemon) =>
         pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
-      console.log('na funcao filterPokemons', this.filteredPokemons);
     } else {
-      this.filteredPokemons = this.pokemons; // Se não houver busca, mostrar todos
+      this.filteredPokemons = this.pokemons;
     }
   }
 }
+
+// @Component({
+//   selector: 'app-poke-list',
+//   standalone: true,
+//   imports: [CommonModule, CardComponent],
+//   templateUrl: './poke-list.component.html',
+//   styleUrls: ['./poke-list.component.scss'],
+// })
+// export class PokeListComponent implements OnInit {
+//   public pokemons: any[] = [];
+//   public filteredPokemons: any[] = [];
+//   public isLoading: boolean = true;
+//   public searchTerm: string = '';
+
+//   constructor(
+//     private pokeApiService: PokeApiService,
+//     private searchService: SearchService // Injetar o SearchService
+//   ) {}
+
+//   ngOnInit(): void {
+//     // Subscrição para obter todos os Pokémons
+//     this.pokeApiService.apiListAllPokemons.subscribe((data) => {
+//       this.pokemons = data.results;
+//       this.filteredPokemons = this.pokemons; // Inicialmente, todos os Pokémons são exibidos
+//       this.isLoading = false;
+//     });
+
+//     // Subscrição para receber atualizações do termo de busca
+//     this.searchService.currentSearchTerm.subscribe((term) => {
+//       this.searchTerm = term;
+//       console.log('Estou na  PokeList:', this.searchTerm); // Log para verificar
+//       this.filterPokemons(); // Filtrar a lista de Pokémons com base no termo de busca
+//     });
+//   }
+
+//   // Método para filtrar os Pokémons com base no searchTerm
+//   filterPokemons(): void {
+//     if (this.searchTerm) {
+//       this.filteredPokemons = this.pokemons.filter((pokemon) =>
+//         pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+//       );
+//       console.log('na funcao filterPokemons', this.filteredPokemons);
+//     } else {
+//       this.filteredPokemons = this.pokemons; // Se não houver busca, mostrar todos
+//     }
+//   }
+// }
--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -1,41 +1,73 @@
-// Importações necessárias para o serviço
 import { Injectable } from '@angular/core'; // Fornece o decorator Injectable para permitir a injeção de dependências
 import { HttpClient } from '@angular/common/http'; // Serviço HTTP para realizar requisições HTTP
 import { map, Observable, tap } from 'rxjs'; // Operadores do RxJS para manipular fluxos de dados assíncronos
 
-// Define que o serviço pode ser injetado em toda a aplicação
 @Injectable({
   providedIn: 'root',
 })
 export class PokeApiService {
-  // URL base para a API de Pokémon com um limite de 6 pokémons
-  private apiUrl: string =
-    'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=21';
+  private apiUrl: string = 'https://pokeapi.co/api/v2/pokemon';
 
-  // Construtor que injeta o serviço HttpClient para realizar requisições HTTP
   constructor(private http: HttpClient) {}
 
-  // Método getter que retorna um Observable contendo a lista de todos os pokémons
-  get apiListAllPokemons(): Observable<any> {
-    return this.http.get<any>(this.apiUrl).pipe(
-      // O operador 'tap' permite executar uma ação com os dados recebidos sem alterá-los
+  // Método para listar pokémons com paginação
+  getPokemonList(offset: number, limit: number): Observable<any> {
+    const url = `${this.apiUrl}/?offset=${offset}&limit=${limit}`;
+    return this.http.get<any>(url).pipe(
       tap((res) => {
-        // Itera sobre os resultados da lista de pokémons
         res.results.map((resPokemons: any) => {
-          // Para cada Pokémon, faz uma requisição para obter mais detalhes
           this.apiGetPokemon(resPokemons.url).subscribe(
-            (res) => (resPokemons.status = res) // Salva os detalhes do Pokémon no objeto original
+            (res) => (resPokemons.status = res)
           );
         });
       })
     );
   }
 
-  // Método público que faz uma requisição HTTP para obter detalhes de um Pokémon específico
+  // Método público para obter detalhes de um Pokémon específico
   public apiGetPokemon(url: string): Observable<any> {
-    return this.http.get<any>(url).pipe(
-      // O operador 'map' transforma a resposta e a retorna
-      map((res) => res)
-    );
+    return this.http.get<any>(url);
   }
 }
+
+// // Importações necessárias para o serviço
+// import { Injectable } from '@angular/core'; // Fornece o decorator Injectable para permitir a injeção de dependências
+// import { HttpClient } from '@angular/common/http'; // Serviço HTTP para realizar requisições HTTP
+// import { map, Observable, tap } from 'rxjs'; // Operadores do RxJS para manipular fluxos de dados assíncronos
+
+// // Define que o serviço pode ser injetado em toda a aplicação
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class PokeApiService {
+//   // URL base para a API de Pokémon com um limite de 6 pokémons
+//   private apiUrl: string =
+//     'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=21';
+
+//   // Construtor que injeta o serviço HttpClient para realizar requisições HTTP
+//   constructor(private http: HttpClient) {}
+
+//   // Método getter que retorna um Observable contendo a lista de todos os pokémons
+//   get apiListAllPokemons(): Observable<any> {
+//     return this.http.get<any>(this.apiUrl).pipe(
+//       // O operador 'tap' permite executar uma ação com os dados recebidos sem alterá-los
+//       tap((res) => {
+//         // Itera sobre os resultados da lista de pokémons
+//         res.results.map((resPokemons: any) => {
+//           // Para cada Pokémon, faz uma requisição para obter mais detalhes
+//           this.apiGetPokemon(resPokemons.url).subscribe(
+//             (res) => (resPokemons.status = res) // Salva os detalhes do Pokémon no objeto original
+//           );
+//         });
+//       })
+//     );
+//   }
+
+//   // Método público que faz uma requisição HTTP para obter detalhes de um Pokémon específico
+//   public apiGetPokemon(url: string): Observable<any> {
+//     return this.http.get<any>(url).pipe(
+//       // O operador 'map' transforma a resposta e a retorna
+//       map((res) => res)
+//     );
+//   }
+// }
